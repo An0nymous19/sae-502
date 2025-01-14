904705f34619e7639c80a7667082346cf826e36c
An0nyme19 <adrien.henry95170@gmail.com>
2025-01-14 09:51:38 +0100

    ajout du fichier CSV

--- generator.py
+++ generator.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+import csv
+
+
+#Répare le fichier csv en remplacant les points virgules par des virgules 
+remplace = open("vacsi-v-fra-2022-01-06-19h05.csv", "r")
+remplace = ''.join([i for i in remplace])
+remplace = remplace.replace(";",",")
+
+a = open("vacsi-v-fra-2022-01-06-19h05.csv","w")
+a.writelines(remplace)
+a.close
  
