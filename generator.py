db8706264eead732d8a07fc570960f57267787a1
An0nyme19 <adrien.henry95170@gmail.com>
2025-01-14 12:55:10 +0100

    code qui permet la maj des fichiers lors de l'exécution

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import csv
+import shutil #déplacer les fichiers 
 
 
 #Répare le fichier csv en remplacant les points virgules par des virgules 
@@ -14,3 +15,11 @@ a.close
 #création de nos fichiers css
 file=open("vacsi-v-fra-2022-01-06-19h05.csv", "r")
 css=open("style.css",'w',encoding="utf-8")
+
+#Supprime l'intégralité des fichiers (met à jour le site avec les nouvelles informations du csv
+try:
+	shutil.rmtree('interface') 
+except:
+	print("Visualisation des données")
+else:
+	print("Mise à jour avec les nouvelles données")
