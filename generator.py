3d1e4633e80d59459907d0eae6160d46bf59d488
Camélia Douicher <douichermassi@gmail.com>
2025-01-15 21:40:47 +0100

    Ajoute de la boucle de génération des informations concernant les doses de vaccins dans la section HTML pour le vaccin Jhonson Jhonson

--- generator.py
+++ generator.py
@@ -380,3 +380,13 @@ html.write("""<table class=table>
         <td class=test>Cumulation quatre</td>
         <td class=test>Cumulation rappel</td
 	</thead>""")
+
+csvreader=csv.DictReader(file)
+def boucle():
+	vaccin='4'
+	for row in csvreader:
+		if row['vaccin']==vaccin:
+			html.write("<tr><td class=date>"+row['jour']+"</td>  <td>"+row['n_dose1']+"</td>  <td>"+row['n_dose2']+"</td>  <td>"+row['n_dose3']+"</td>  <td>"+row['n_dose4']+"</td>  <td>"+row['n_rappel']+"</td>  <td>"+row['n_cum_dose1']+"</td>  <td>"+row['n_cum_dose2']+"</td>  <td>"+row['n_cum_dose3']+"</td>  <td>"+row['n_cum_dose4']+"</td>  <td>"+row['n_cum_rappel']+"</td></tr>")
+boucle()
+html.write("</tbody></table></body></html>")
+html.close()
